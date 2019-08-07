daff22a22120b970a9f6b4802154a84046956b17
BuddahLD <danylo.oliinyk@gmail.com>
2019-08-07 10:38:20 +0300

    Copyright update, exception on migration failure, removed odd tests

--- ethree-enclave/src/main/java/com/virgilsecurity/android/ethreeenclave/interaction/EThree.kt
+++ ethree-enclave/src/main/java/com/virgilsecurity/android/ethreeenclave/interaction/EThree.kt
@@ -1,36 +1,34 @@
 /*
+ * Copyright (c) 2015-2019, Virgil Security, Inc.
  *
- *  * Copyright (c) 2015-2019, Virgil Security, Inc.
- *  *
- *  * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
- *  *
- *  * All rights reserved.
- *  *
- *  * Redistribution and use in source and binary forms, with or without
- *  * modification, are permitted provided that the following conditions are met:
- *  *
- *  *     (1) Redistributions of source code must retain the above copyright notice, this
- *  *     list of conditions and the following disclaimer.
- *  *
- *  *     (2) Redistributions in binary form must reproduce the above copyright notice,
- *  *     this list of conditions and the following disclaimer in the documentation
- *  *     and/or other materials provided with the distribution.
- *  *
- *  *     (3) Neither the name of virgil nor the names of its
- *  *     contributors may be used to endorse or promote products derived from
- *  *     this software without specific prior written permission.
- *  *
- *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
  *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *     (3) Neither the name of virgil nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.virgilsecurity.android.ethreeenclave.interaction
@@ -43,6 +41,7 @@ import com.virgilsecurity.android.common.interaction.KeyManagerLocal
 import com.virgilsecurity.android.common.model.Result
 import com.virgilsecurity.sdk.androidutils.storage.AndroidKeyEntry
 import com.virgilsecurity.sdk.androidutils.storage.AndroidKeyStorage
+import com.virgilsecurity.sdk.crypto.exceptions.KeyStorageException
 import com.virgilsecurity.sdk.jwt.Jwt
 import com.virgilsecurity.sdk.jwt.accessProviders.CachingJwtProvider
 import com.virgilsecurity.sdk.jwt.contract.AccessTokenProvider
@@ -89,6 +88,8 @@ private constructor(
                         keyStorageAndroid.store(keyEntryAndroid)
                     } catch (throwable: Throwable) {
                         keyStorageAndroid.names().forEach { keyStorageAndroid.delete(it) }
+                        throw KeyStorageException("Error while migrating keys from legacy key "
+                        + "storage to the new one. All keys are restored in legacy key storage.")
                     }
                 }
             }
@@ -107,7 +108,11 @@ private constructor(
          * if something went wrong.
          *
          * To start execution of the current function, please see [Result] description.
+         *
+         * May throw [KeyStorageException] if error occurred while initializing Android Key Storage
+         * or on keys migration failure.
          */
+        @Throws(KeyStorageException::class)
         @JvmStatic fun initialize(context: Context,
                                   onGetTokenCallback: OnGetTokenCallback,
                                   alias: String = "VirgilAndroidKeyStorage",
--- testsenclave/src/androidTest/java/com/virgilsecurity/android/testsenclave/EThreeEnclaveTest.kt
+++ testsenclave/src/androidTest/java/com/virgilsecurity/android/testsenclave/EThreeEnclaveTest.kt
@@ -1,36 +1,34 @@
 /*
+ * Copyright (c) 2015-2019, Virgil Security, Inc.
  *
- *  * Copyright (c) 2015-2019, Virgil Security, Inc.
- *  *
- *  * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
- *  *
- *  * All rights reserved.
- *  *
- *  * Redistribution and use in source and binary forms, with or without
- *  * modification, are permitted provided that the following conditions are met:
- *  *
- *  *     (1) Redistributions of source code must retain the above copyright notice, this
- *  *     list of conditions and the following disclaimer.
- *  *
- *  *     (2) Redistributions in binary form must reproduce the above copyright notice,
- *  *     this list of conditions and the following disclaimer in the documentation
- *  *     and/or other materials provided with the distribution.
- *  *
- *  *     (3) Neither the name of virgil nor the names of its
- *  *     contributors may be used to endorse or promote products derived from
- *  *     this software without specific prior written permission.
- *  *
- *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- *  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- *  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- *  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- *  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- *  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
  *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright notice, this
+ *     list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *
+ *     (3) Neither the name of virgil nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package com.virgilsecurity.android.testsenclave
@@ -39,7 +37,6 @@ import android.support.test.runner.AndroidJUnit4
 import com.virgilsecurity.android.common.callback.OnGetTokenCallback
 import com.virgilsecurity.android.ethreeenclave.interaction.EThree
 import com.virgilsecurity.android.testsenclave.utils.TestConfig
-import com.virgilsecurity.android.testsenclave.utils.TestConfig.Companion.KEYSTORE_NAME
 import com.virgilsecurity.sdk.androidutils.storage.AndroidKeyEntry
 import com.virgilsecurity.sdk.androidutils.storage.AndroidKeyStorage
 import com.virgilsecurity.sdk.common.TimeSpan
@@ -48,7 +45,6 @@ import com.virgilsecurity.sdk.crypto.VirgilCrypto
 import com.virgilsecurity.sdk.jwt.JwtGenerator
 import com.virgilsecurity.sdk.storage.DefaultKeyStorage
 import org.junit.Assert.assertEquals
-import org.junit.Before
 import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -141,7 +137,50 @@ class EThreeEnclaveTest {
             }
         }, isAuthenticationRequired = false, alias = alias).get()
 
+        assertEquals(0, keyStorageDefault.names().size)
         assertEquals(10, androidKeyStorage.names().size)
+    }
+
+    @Test fun migration_using_legacy_ethree_and_register_new() {
+        val alias = UUID.randomUUID().toString()
+
+        val keyStorageDefault = DefaultKeyStorage(TestConfig.context.filesDir.absolutePath,
+                                                  KEYSTORE_NAME)
+        assertEquals(0, keyStorageDefault.names().size)
+
+        val identityLegacy = UUID.randomUUID().toString()
+
+        val eThreeLegacy = com.virgilsecurity.android.ethree.interaction.EThree.initialize(
+            TestConfig.context, object : OnGetTokenCallback {
+                override fun onGetToken(): String {
+                    return generateToken(identityLegacy)
+                }
+            }
+        ).get()
+
+        eThreeLegacy.register().execute()
+
+        assertEquals(1, keyStorageDefault.names().size)
+
+        val androidKeyStorage = AndroidKeyStorage.Builder(alias)
+                .isAuthenticationRequired(false)
+                .onPath(TestConfig.context.filesDir.absolutePath)
+                .build()
+        assertEquals(0, androidKeyStorage.names().size)
+
+        val identity = UUID.randomUUID().toString()
+
+        val eThreeNew = EThree.initialize(TestConfig.context, object : OnGetTokenCallback {
+            override fun onGetToken(): String {
+                return generateToken(identity)
+            }
+        }, isAuthenticationRequired = false, alias = alias).get()
+
+        assertEquals(0, keyStorageDefault.names().size)
+        assertEquals(1, androidKeyStorage.names().size)
+
+        eThreeNew.register().execute()
+        assertEquals(2, androidKeyStorage.names().size)
     }
 
     private fun generateToken(identity: String): String =
